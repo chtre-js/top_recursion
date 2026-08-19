5ba7b5bcd777a4c5c80a6772c75c14c7dfacdcda
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-19 20:38:29 +0200

    test: add sorting test

--- merge-sort/mergeSort.spec.js
+++ merge-sort/mergeSort.spec.js
@@ -3,3 +3,7 @@ import mergeSort from "./mergeSort.js"
 test("mergeSort([]) returns an empty string", () => {
   expect(mergeSort([])).toStrictEqual([])
 })
+
+test("mergeSort([2,1] returns [1,2]", () => {
+  expect(mergeSort([2,1])).toStrictEqual([1,2])
+})
