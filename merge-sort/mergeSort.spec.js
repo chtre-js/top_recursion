5f9f0015741921899f51438ee16afb93e33496b1
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-19 20:54:05 +0200

    Add general test

--- merge-sort/mergeSort.spec.js
+++ merge-sort/mergeSort.spec.js
@@ -1,9 +1,13 @@
-import mergeSort from "./mergeSort.js"
+import mergeSort from "./mergeSort.js";
 
 test("mergeSort([]) returns an empty string", () => {
-  expect(mergeSort([])).toStrictEqual([])
-})
+  expect(mergeSort([])).toStrictEqual([]);
+});
 
 test("mergeSort([2,1] returns [1,2]", () => {
-  expect(mergeSort([2,1])).toStrictEqual([1,2])
-})
+  expect(mergeSort([2, 1])).toStrictEqual([1, 2]);
+});
+
+test("sort is correct", () => {
+  expect(mergeSort([5, 4, 3, 2, 1])).toStrictEqual([1, 2, 3, 4, 5]);
+});
