e5440affc38b91f29a4eaff3397e64dba994cfab
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-19 18:54:40 +0200

    test: test that the returned array are correct

--- fibs.spec.js
+++ fibs.spec.js
@@ -1,9 +1,13 @@
 import { fibs } from "./fibs.js";
 
-test("fibs of 0 is 0", () => {
-  expect(fibs(0)).toBe(0)
-})
+test("fibs of 1 is [0, 1]", () => {
+  expect(fibs(1)).toStrictEqual([0]);
+});
 
-test("fibs of 5 is 5", () => {
-  expect(fibs(5)).toBe(5)
-})
+test("fibs of 2 is [0,1,1]", () => {
+  expect(fibs(2)).toStrictEqual([0, 1]);
+});
+
+test("fibs of 8 is [0, 1, 1, 2, 3, 5, 8, 13]", () => {
+  expect(fibs(8)).toStrictEqual([0,1,1,2,3,5,8,13]);
+});
