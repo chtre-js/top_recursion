0efc9725d4f94ce4dcdcfc242f6ca0c77102f8eb
chtre-js <227361371+chtre-js@users.noreply.github.com>
2026-08-19 18:55:18 +0200

    feat: return an array of length n

--- fibs.js
+++ fibs.js
@@ -1,8 +1,13 @@
 function fibs(n) {
-  if (n < 2) {
-    return n;
+  if (n === 1) {
+    return [0];
   }
-  return fibs(n - 1) + fibs(n - 2);
+  if (n === 2) {
+    return [0, 1];
+  }
+  const arr = fibs(n-1);
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return arr
 }
 
-export { fibs }
+export { fibs };
